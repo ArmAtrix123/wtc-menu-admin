e36c2c5a8bb914002d4fcbb3564e1c589e951b2e
Merfy <96495867+ArmAtrix123@users.noreply.github.com>
2023-03-13 17:40:12 +0300

    create: auth screen

---
+++ js/script.js
@@ -0,0 +1,41 @@
+window.addEventListener('DOMContentLoaded', function() {
+    function closeModal(modalSelector) {
+        const modal = document.querySelector(modalSelector);
+
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        // modal.style.transform = 'scale(0)';
+        document.body.style.overflow = '';
+    }
+
+    function openModal(modalSelector) {
+        const modal = document.querySelector(modalSelector);
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        // modal.style.transform = 'scale(1)';
+        document.body.style.overflow = 'hidden';
+    }
+
+    function modal(triggerSelector, modalSelector) {
+        const 
+            modalTrigger = document.querySelectorAll(triggerSelector),
+            modal = document.querySelector(modalSelector);
+
+        modalTrigger.forEach(btn => {
+            btn.addEventListener('click', () => openModal(modalSelector));
+        });
+
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal || e.target.getAttribute('data-close') == "") {
+                closeModal(modalSelector);
+            }
+        });
+
+        document.addEventListener('keydown', (e) => {
+            if (e.code === "Escape" && modal.classList.contains('show')) { 
+                closeModal(modalSelector);
+            }
+        });
+    }
+    modal('[data-modal]', '.wrapper');
+})
